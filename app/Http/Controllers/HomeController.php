fc12ea587ade59841e722535e99ee69fbf8722f4
momartallagning <gning.mamadou@ugb.edu.sn>
2020-08-18 12:58:36 +0000

    l’authentification

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Show home page
      *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        $products = Product::whereActive(true)->get();
+
+        return view('home', compact('products'));
     }
 }
